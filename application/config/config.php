cd2c65a8f462f81db987b060da6601e85078a08c
mcnuggets-lab <mcnuggets23@gmail.com>
2015-12-07 23:01:44 +0800

    Commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/polls/';
+$config['base_url'] = 'http://phptest-mcnuggetslab.rhcloud.com/';
 
 /*
 |--------------------------------------------------------------------------
